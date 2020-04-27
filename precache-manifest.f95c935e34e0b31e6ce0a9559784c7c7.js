9e84ed1f49fcadcb4a582ae5932e253fe600698d
pratik <pratikagrawal2008@gmail.com>
2020-04-27 19:34:54 +0530

    Updates

---
+++ precache-manifest.f95c935e34e0b31e6ce0a9559784c7c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479b9276e9881ddaf33469e46a57b7f5",
+    "url": "/bhangadiyas.github.io/index.html"
+  },
+  {
+    "revision": "d507d83d817b7cff041e",
+    "url": "/bhangadiyas.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7d1ea724babeb1966341",
+    "url": "/bhangadiyas.github.io/static/js/2.02cf0853.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bhangadiyas.github.io/static/js/2.02cf0853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d507d83d817b7cff041e",
+    "url": "/bhangadiyas.github.io/static/js/main.051a040f.chunk.js"
+  },
+  {
+    "revision": "80017bd5e219348782f7",
+    "url": "/bhangadiyas.github.io/static/js/runtime-main.1181c7a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bhangadiyas.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
